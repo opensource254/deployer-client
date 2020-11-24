21a461f20a9fc3e9c52e6cad88c2b20b61735f56
Stanley Masinde <stanmasinde@gmail.com>
2020-11-24 13:48:35 +0300

    Updated the timeout for command excecution Changes: 	modified:   pages/config/_id.vue

--- pages/config/_id.vue
+++ pages/config/_id.vue
@@ -156,9 +156,13 @@ export default {
       try {
         this.command.running = true
         await this.$axios.get('/api/csrf-cookie')
-        const { data } = await this.$axios.post('/api/command/run', {
-          command: this.app.id,
-        })
+        const { data } = await this.$axios.post(
+          '/api/command/run',
+          {
+            command: this.app.id,
+          },
+          { timeout: 1000000000000000 }
+        )
         this.command.results = true
         this.command.body = data
         this.command.running = false
